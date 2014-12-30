ef13c5e577c85322c536d8a8295f67ef5ad0aaae
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2014-12-30 17:03:07 +0900

    Update diff script.

--- diff.py
+++ diff.py
@@ -3,7 +3,7 @@
 import os, subprocess
 
 if __name__ == "__main__":
-    for passname in ["CSE", "Inlining", "Tailcall"]:
+    for passname in ["Renumber", "Constprop", "Deadcode", "CSE", "Tailcall", "Inlining"]:
         output1 = subprocess.check_output("diff ../backend/%sproof.v backend/%sproof_linker.v | grep \"^>\" | wc -l" % (passname, passname), shell=True)
         output2 = subprocess.check_output("diff ../backend/%sproof.v backend/%sproof_linker.v | grep \"^> (\*\" | wc -l" % (passname, passname), shell=True)
         print("%s:\t%d \t- 1.5*%d \t= %d" % (passname, int(output1), int(output2), int(int(output1) - int(output2) * 1.5)))
