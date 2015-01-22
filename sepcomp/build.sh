b7ada8c11022ff283e08ca42aa53fcce92867d66
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2015-01-23 01:09:36 +0900

    Add README and build script.

--- sepcomp/build.sh
+++ sepcomp/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  jobs=1
+else
+  jobs=$1
+fi
+
+cd ..
 make -f sepcomp/Makefile depend &&
-make -f sepcomp/Makefile -j $1 proof &&
+make -f sepcomp/Makefile -j $jobs proof &&
 make -f sepcomp/Makefile extraction &&
 make -f sepcomp/Makefile ccomp &&
 make -f sepcomp/Makefile runtime &&
